be0c581e51587fdb329b3e1cfe335722cda8a7c9
carmenvillalobos <carmvilla15@gmail.com>
2023-07-18 14:34:05 -0700

    first commit

---
+++ index.js
@@ -0,0 +1,29 @@
+// You are given the heads of two sorted linked lists list1 and list2.
+
+// Merge the two lists in a one sorted list. The list should be made by splicing together the nodes of the first two lists.
+
+// Return the head of the merged linked list.
+
+
+
+var mergeTwoLists = function(list1, list2) {
+    const dummy = new ListNode(-Infinity)
+    let prev = dummy
+
+    while (list1 && list2){
+      if(list1.val <= list2.val){
+        prev.next = list1 //go to list1
+        prev = list1 //this is where the pointer(previous) goes
+        list1 = list1.next //move to next value in list
+      } else {
+        prev.next =list2 //go to list2
+        prev = list2
+        list2 = list2.next
+      }
+    }
+
+    if(!list1) prev.next = list2
+    if(!list2) prev.next = list1
+
+    return dummy.next
+}
